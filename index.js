75d590dd6e34ef79f17a5eab5c43b37d61c59316
Paulo_Araya <pauloarayademetri@gmail.com>
2021-03-27 10:27:17 -0300

    Nuevos cambios con JS

---
+++ index.js
@@ -0,0 +1,8 @@
+const saludo = "hola mundo";/* "const" se refiere a una variable */
+
+console.log(typeof saludo);/* "console.log" hace referencia a una función con la variable que está en parentesis, "strig" se refiere a hilera de caractere */
+
+const obj1 = {a: "3", b: 4}/* al incluir el valor entre comillas, se considera el valor como un string */
+const obj2 = {a: 5, b: 6, c: 7}
+
+console.log(obj1)
--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 let numero = 50;
 
-let numero1 = 150
+let numero1 = 150;
 
-var resultado = numero + numero1
+var resultado = numero + numero1;
 
 console.log(resultado);
 
